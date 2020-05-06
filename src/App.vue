a2584bffca5530a5ebee389088320a7d5f07102e
thekevinT <kevinftipantiza@gmail.com>
2020-05-06 18:44:39 -0500

    insertando nav de bulma

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,23 @@
   <div id="app">
     <div id="nav">
       HOLA KEVIN PAGINA PRINCIPAL
-    
+      <!-- Declarar etiqueta creada -->
+    <Navigation></Navigation>
     </div>
     <router-view/>
   </div>
 </template>
+
+<!-- poner script para que funcione el nav -->
+
+<script>
+//@ significa que busaca desde el src
+import Navigation from '@/components/layout/Navigation'
+
+export default {
+  //declarar componentes
+  components:{
+    Navigation
+  }
+}
+</script>
