76cd7b074f6f88c96a2e19b520949a9cac72360e
thekevinT <kevinftipantiza@gmail.com>
2020-05-06 18:54:34 -0500

    arreglando nav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      HOLA KEVIN PAGINA PRINCIPAL
+  
       <!-- Declarar etiqueta creada -->
     <Navigation />
     </div>
--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
+   <router-link class="navbar-item" to='/' > 
+      <h3 class="title is-3">LOGO</h3>
+    </router-link>
 
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
@@ -14,35 +14,11 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">
+      <router-link class="navbar-item" to='/'>
         Home
-      </a>
+    </router-link> 
 
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
+      
     </div>
 
     <div class="navbar-end">
