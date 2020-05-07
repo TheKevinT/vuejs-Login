716817923c6867fa807a0bf4e287a4303dbb03bd
thekevinT <kevinftipantiza@gmail.com>
2020-05-06 20:19:40 -0500

    creacion de login

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../views/Home.vue'
 //importar vistas
 import Login from '../views/Auth/Login.vue'
 import Register from '../views/Auth/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -22,7 +23,13 @@ const routes = [{
         path: '/register',
         name: 'register',
         component: Register
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard
     }
+
 
 ]
 
--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,11 +1,82 @@
 <template>
-    <div>
-        LOGIN !!!
+    <div class="container mt-50" >
+        <div class="columns">
+            <div class="column is-6 is-offset-3">
+
+                <h3 class="title is-3">INICIAR SESION</h3><hr>
+            
+                <form action="#" @submit.prevent="login">
+                
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com" v-model="email">
+                        </div>
+                    </div>
+
+
+                     <div class="field">
+                        <label class="label">Contraseña</label>
+                        <div class="control">
+                            <input class="input" type="password" v-model="password">
+                        </div>
+                    </div>
+                
+                <button type="submit" class="button is-primary">Iniciar Sesión</button>
+                </form>
+
+
+<!-- ALERTA-->
+                <div class="notification is-danger mt-10" v-if="error">
+               {{ error }}
+                </div>
+
+
+
+
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
+//importar firebase
+import '@/firebase/init'
+import firebase from 'firebase'
+
 export default {
-    name:'Login'
+    //capturar valores de los campor
+    data() {
+        return {
+          
+            email: '',
+            password:'',
+            error: ''
+        }
+    },
+    name:'Login',
+    methods: {
+        //para que no se envie el formulario al dar clic vacio
+        login(){
+
+            this.error =''
+
+            if( this.email && this.password){
+                //enviamos formulario
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                .then(user =>{
+                    this.$router.push({name: 'dashboard'})
+                    console.log(user);
+                }).catch(err =>{
+                    this.error = err.message
+                })
+            
+            }else{
+                this.error = 'Todos los campos son requeridos'
+            }
+        }
+    }
 }
 </script>
