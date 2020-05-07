ba6620ae024cc7dace84f6b64dec59be9e7136dd
thekevinT <kevinftipantiza@gmail.com>
2020-05-06 19:24:26 -0500

    Crear formulario registrar

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,6 +1,39 @@
 <template>
-    <div>
-        Register .....
+    <div class="container">
+        <div class="columns">
+            <div class="column is-6 is-offset-3">
+
+                <h3 class="title is-3">CREAR UNA CUENTA</h3><hr>
+            
+                <form action="#">
+                
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <input class="input" type="text" placeholder="e.g Alex Smith">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+                        </div>
+                    </div>
+
+
+                     <div class="field">
+                        <label class="label">Contraseña</label>
+                        <div class="control">
+                            <input class="input" type="password" >
+                        </div>
+                    </div>
+                
+                <button type="submit" class="button is-primary">Registrarme</button>
+                </form>
+
+            </div>
+        </div>
     </div>
 </template>
 
